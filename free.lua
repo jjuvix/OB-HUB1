2a7ab22a89e68fc5ddcbcf87892643dd4f5c5b0f
OB HUB <87583451+jjuvix@users.noreply.github.com>
2022-05-08 09:05:05 -0700

    Update free.lua

--- free.lua
+++ free.lua
@@ -2,13 +2,11 @@ _G.ToggleColor = Color3.fromRGB(255,0,0)
 _G.ButtonColor = Color3.fromRGB(0,255,0)
 _G.SliderColor = Color3.fromRGB(0,0,255)
 
-local library = loadstring(game:HttpGet(('https://pastebin.com/raw/FsJak6AT')))() -- It's obfuscated, I won't let you see my ugly coding skills. =)
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))() -- It's obfuscated, I won't let you see my ugly coding skills. =)
 
 local w = library:CreateWindow("OB HUB | OUTDOOR BRAWLING")
 
 local b = w:CreateFolder("MAIN")
-
-b:Label("NOT DONE!",Color3.fromRGB(38,38,38),Color3.fromRGB(0,216,111)) --BgColor,TextColor
 
 b:Button("Auto Gloves",function()
     fireclickdetector(game.Workspace.Glovess.ClickDetector)
@@ -59,40 +57,18 @@ b:Button("Jump Height", function()
     return cd(v);
 end);
 end)
-b:Toggle("Toggle",function(bool)
-    shared.toggle = bool
-    print(shared.toggle)
-end)
 
-b:Slider("Walkspeed",16,100,true,function(value) --MinValue,MaxValue,Precise
+b:Slider("Walkspeed",{
+    
+    16,
+    100,
+    precise = true,
+    },function(value) --MinValue,MaxValue,Precise
     while wait() do
        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed.Value = value 
     end
 end)
 
-b:Slider("Jump Power",50, 200, true, function(value)
-        while wait() do
-       game.Players.LocalPlayer.Character.Humanoid.JumpHeight.Value = value 
-       game.Players.LocalPlayer.Character.Humanoid.JumpPower.Value = value
-    end 
+b:Button("No Kill Bricks", function()
+    print("no kill bricks") 
 end)
-
-b:Dropdown("Dropdown",{"A","B","C"},true,function(mob) --Replace the Dropdown name with the selected one(A,B,C)
-    print(mob)
-end)
-
-b:Bind("Bind",Enum.KeyCode.C,function() --Default bind
-    print("Yes")
-end)
-
-b:ColorPicker("ColorPicker",Color3.fromRGB(255,0,0),function(color)
-    print(color)
-end)
-
-b:Box("Box","number",function(value) -- "number" or "string"
-    print(value)
-end)
-
-b:DestroyGUI()
-
-b:GuiSettings()
